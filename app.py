1fb125e94be8bdf36a6b7d88b7d0f1f85599619a
Jenda Brands <jbrands@schubergphilis.com>
2022-11-23 16:20:23 +0100

    Updated to latest labeler version

--- app.py
+++ app.py
@@ -1,53 +1,152 @@
-import logging
-from awsenergylabelerlib import (DestinationPath,
-                                 DataExporter)
 from chalice import Chalice, Rate
-from datetime import datetime
 
-from chalicelib.exceptions import (InvalidDestinationPath)
-from chalicelib.retrievers import (get_landing_zone_reporting_data,
-                                   get_account_reporting_data)
-from chalicelib.validators import (get_mutually_exclusive,
-                                   get_required_variable,
-                                   get_environment_boolean,
-                                   get_log_level)
+import datetime
+import json
+import logging
+
+from art import text2art
+from terminaltables import AsciiTable
+from awsenergylabelerlib import DataExporter, DEFAULT_SECURITY_HUB_FRAMEWORKS
+from awsenergylabelerlib._version import __version__ as lib_version
+from awsenergylabelercli._version import __version__ as cli_version
+
+from awsenergylabelercli import (calculate_file_hash,
+                                 get_account_reporting_data,
+                                 get_arguments,
+                                 get_zone_reporting_data,
+                                 setup_logging)
+from awsenergylabelercli.entities import MetadataEntry, Metadata
+
+
+def create_execution_metadata(args):
+    """Creates the execution metadata.
+
+    Args:
+        args: The arguments provided by argparse as set by the user.
+
+    Returns:
+        The calculated metadata.
+
+    """
+    metadata = Metadata()
+    metadata.add_entry(MetadataEntry(title='Default Account Thresholds Overwritten:',
+                                     value=bool(args.account_thresholds),
+                                     is_report_entry=True))
+    if args.account_thresholds:
+        metadata.add_entry(MetadataEntry(title='Account Thresholds Provided:',
+                                         value=args.account_thresholds,
+                                         is_report_entry=False))
+    metadata.add_entry(MetadataEntry(title='Default Zone Thresholds Overwritten:',
+                                     value=bool(args.zone_thresholds),
+                                     is_report_entry=True))
+    if args.zone_thresholds:
+        metadata.add_entry(MetadataEntry(title='Zone Thresholds Provided:',
+                                         value=args.zone_thresholds,
+                                         is_report_entry=False))
+    metadata.add_entry(MetadataEntry(title='Default Security Hub Query Filter Overwritten:',
+                                     value=bool(args.security_hub_query_filter),
+                                     is_report_entry=True))
+    if args.security_hub_query_filter:
+        metadata.add_entry(MetadataEntry(title='Security Hub Query Filter Provided:',
+                                         value=args.security_hub_query_filter,
+                                         is_report_entry=False))
+
+    if set(args.frameworks) != set(DEFAULT_SECURITY_HUB_FRAMEWORKS):
+        metadata.add_entry(MetadataEntry(title='Default Frameworks Overwritten:',
+                                         value=True,
+                                         is_report_entry=True))
+        metadata.add_entry(MetadataEntry(title='Frameworks Provided:',
+                                         value=args.frameworks,
+                                         is_report_entry=False))
+    metadata.add_entry(MetadataEntry(title='Library Version:',
+                                     value=lib_version,
+                                     is_report_entry=True))
+    metadata.add_entry(MetadataEntry(title='Cli Version:',
+                                     value=cli_version,
+                                     is_report_entry=True))
+    return metadata
+
+
+def _get_reporting_arguments(args):
+    start_run_time = datetime.datetime.now()
+    execution_metadata = create_execution_metadata(args)
+    method_arguments = {'region': args.region,
+                        'frameworks': args.frameworks,
+                        'allowed_regions': args.allowed_regions,
+                        'denied_regions': args.denied_regions,
+                        'report_closed_findings_days': args.report_closed_findings_days,
+                        'report_suppressed_findings': args.report_suppressed_findings,
+                        'account_thresholds': args.account_thresholds,
+                        'export_all_data_flag': args.export_all,
+                        'security_hub_query_filter': args.security_hub_query_filter,
+                        'log_level': args.log_level,
+                        'metadata': execution_metadata}
+    if args.single_account_id:
+        get_reporting_data = get_account_reporting_data
+        method_arguments.update({'account_id': args.single_account_id})
+    else:
+        zone_type = 'organizations_zone' if args.organizations_zone_name else 'audit_zone'
+        zone_name = args.organizations_zone_name or args.audit_zone_name
+        get_reporting_data = get_zone_reporting_data
+        method_arguments.update({'zone_name': zone_name,
+                                 'allowed_account_ids': args.allowed_account_ids,
+                                 'denied_account_ids': args.denied_account_ids,
+                                 'zone_type': zone_type,
+                                 'zone_thresholds': args.zone_thresholds})
+    report_data, exporter_arguments = get_reporting_data(**method_arguments)
+    execution_metadata = exporter_arguments.get('metadata')
+    report_data.extend(execution_metadata.report_table)
+    end_run_time = datetime.datetime.now()
+    execution_metadata.add_entry(MetadataEntry(title='Date and time of end of execution:',
+                                               value=str(end_run_time),
+                                               is_report_entry=True))
+    execution_metadata.add_entry(MetadataEntry(title='Duration of run:',
+                                               value=str(end_run_time - start_run_time),
+                                               is_report_entry=True))
+    execution_metadata.add_entry(MetadataEntry(title='Hash:',
+                                               value=calculate_file_hash(
+                                                   json.dumps(execution_metadata.data).encode('utf-8')),
+                                               is_report_entry=False))
+    exporter_arguments.update({'metadata': execution_metadata.data})
+    return report_data, exporter_arguments
+
+
+def report(report_data, to_json=False):
+    """Report to table or json."""
+    if to_json:
+        data = {key.replace(':', '').replace(' ', '_').lower(): value for key, value in dict(report_data).items()}
+        print(json.dumps(data, indent=2))
+        return None
+    table_data = [['Energy label report']]
+    table_data.extend(report_data)
+    table = AsciiTable(table_data)
+    print(table.table)
+    return None
 
 
 def main(logger):
     """Main method."""
-    landing_zone_name, single_account_id = get_mutually_exclusive('LANDING_ZONE_NAME', 'SINGLE_ACCOUNT_ID')
-    region = get_required_variable('REGION')
-    allowed_account_ids, denied_account_ids = get_mutually_exclusive('ALLOWED_ACCOUNT_IDS', 'DENIED_ACCOUNT_IDS')
-    allowed_regions, denied_regions = get_mutually_exclusive('ALLOWED_REGIONS', 'DENIED_REGIONS')
-    export_all_data_flag = get_environment_boolean('EXPORT_ALL_DATA', default_value=False)
-    log_level = get_log_level('LOG_LEVEL')
-    export_path = get_required_variable('EXPORT_PATH')
-    if DestinationPath(export_path).type != 's3':
-        raise InvalidDestinationPath(export_path)
-    date_export_path = f"{export_path}{datetime.now().year}/{datetime.now().month}/{datetime.now().day}/"
-    logger.setLevel(getattr(logging, log_level))
+    args = get_arguments()
+    logging.getLogger('botocore').setLevel(logging.ERROR)
+    for entity in ['account', 'zone']:
+        if getattr(args, f'{entity}_thresholds'):
+            logger.warning(f'{entity.capitalize()} thresholds have been overwritten, '
+                           f'configuration will be reported on the output.')
+    if not args.frameworks:
+        logger.info('No frameworks have been provided for filtering.')
     try:
-        if landing_zone_name:
-            report_data, exporter_arguments = get_landing_zone_reporting_data(landing_zone_name=landing_zone_name,
-                                                                              region=region,
-                                                                              allowed_account_ids=allowed_account_ids,
-                                                                              denied_account_ids=denied_account_ids,
-                                                                              allowed_regions=allowed_regions,
-                                                                              denied_regions=denied_regions,
-                                                                              export_all_data_flag=export_all_data_flag)
-        else:
-            report_data, exporter_arguments = get_account_reporting_data(account_id=single_account_id,
-                                                                         region=region,
-                                                                         allowed_regions=allowed_regions,
-                                                                         denied_regions=denied_regions,
-                                                                         export_all_data_flag=export_all_data_flag)
-        logger.info(f'Trying to export data to the requested path : {date_export_path}')
-        exporter = DataExporter(**exporter_arguments)
-        exporter.export(date_export_path)
-    except Exception as msg:
+        print(text2art("AWS Energy Labeler"))
+        report_data, exporter_arguments = _get_reporting_arguments(args)
+        if args.export_path:
+            logger.info(f'Trying to export data to the requested path : {args.export_path}')
+            exporter = DataExporter(**exporter_arguments)
+            exporter.export(args.export_path)
+        report(report_data, args.to_json)
+        status_code = 0
+    except Exception as msg:  # pylint: disable=broad-except
         logger.error(msg)
-        return False
-    return True
+        status_code = 1
+    return status_code
 
 
 app = Chalice(app_name="EnergyLabeler")
